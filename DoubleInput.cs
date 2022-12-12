5b497b7714710aa4f26ecfa50c677bde3fc142aa
Ksenia <83668537+maminadochka@users.noreply.github.com>
2022-12-12 21:03:26 +0300

    tested files

---
+++ DoubleInput.cs
@@ -0,0 +1,20 @@
+﻿public class SecondClass
+{
+    public void SecondMethod()
+    {
+
+    }
+}
+
+public class ThirdClass
+{
+    public void ThirdMethod()
+    {
+
+    }
+
+    public void ThirdMethod(double a)
+    {
+        
+    }
+}
---
+++ Input.cs
@@ -0,0 +1,20 @@
+namespace SimpleNamespace
+{
+    public class FirstClass
+    {
+        public void FirstMethod()
+        {
+
+        }
+
+        public void FirstMethod(int a)
+        {
+
+        }
+
+        private void PrivateMethod()
+        {
+            
+        }
+    }
+}
